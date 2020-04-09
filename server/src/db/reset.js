277e1042627ae7e75b5e238088a8fd0a59572920
Gwangsu Shin <gsshin@firma-solutions.com>
2020-04-09 17:48:41 +0900

    sequelize reset script

--- server/src/db/reset.js
+++ server/src/db/reset.js
@@ -1,10 +1,18 @@
-import conn, { close } from './index';
+import { close } from './index';
 
+import Account from '../account/model';
+import Analytics from '../analytics/model';
+import Block from '../block/model';
+import Transaction from '../transaction/model';
+import Candidate from '../candidate/model';
 
-
-const reset = () => {
-  conn.sync({force: true}).then(close);
-}
+const reset = () => [Transaction, Analytics, Candidate, Account, Block].reduce(
+  (promise, model) => promise.then(() => model.destroy({
+    where: {},
+    // truncate: true,
+  })),
+  Promise.resolve(),
+).then(close);
 
 export default reset;
 
